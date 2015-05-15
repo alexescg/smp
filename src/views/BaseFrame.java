b97191d3568917248f203b0e115ddce7e50e2186
Alex Escobedo <aeg0204@gmail.com>
2015-05-15 08:57:08 -0600

    BaseFrame, gitignore agregados

--- src/views/BaseFrame.java
+++ src/views/BaseFrame.java
@@ -1,16 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views;
 
-import javax.swing.JFrame;
-
 /**
- *
- * @author ALO
+ * Frame Base para la creacion del proyecto.
+ * @author Alejandro Escobedo Garcia
  */
-public class BaseFrame extends JFrame{
-    
+public class BaseFrame extends javax.swing.JFrame {
+
+    /**
+     * Crear nueva Frame
+     */
+    public BaseFrame() {
+        initComponents();
+    }
+
+    /**
+     * Metodo para iniciar los componentes Base del frame.
+     * initComponents() Cambios generales a las vistas van aqui.
+     */
+    private void initComponents() {
+        //Exit con Close
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        //Ancho x Largo
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        
+        /** If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * @see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         **/
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(BaseFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Crear y mostrar Form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new BaseFrame().setVisible(true);
+        });
+    }
 }

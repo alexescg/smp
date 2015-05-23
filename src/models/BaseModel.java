ffc73a88cc0de9d854920d3d69b69d047a8a353e
Alex Escobedo <aeg0204@gmail.com>
2015-05-23 01:29:01 -0600

    Formato de BaseModel

--- src/models/BaseModel.java
+++ src/models/BaseModel.java
@@ -2,36 +2,34 @@ package models;
 
 /**
  * Modelo que sirve como base para todos los demas modelos
+ *
  * @author Alejandro Escobedo
  */
 public class BaseModel {
-    
+
     /**
      * Metodo para checar si algun valor es numerico.
+     *
      * @param str
-     * @return 
+     * @return
      */
-    public static Boolean isNumeric(String str)  
-{   
-  try   
-  {   
-    double d = Double.parseDouble(str);  
-  }   
-  catch(NumberFormatException nfe)  
-  {   
-    return false;   
-  }   
-  return true;   
-} 
+    public static Boolean isNumeric(String str) {
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     /**
      * Metodo para revisar que una cadena no sea nula ni este vacia.
+     *
      * @param str
-     * @return 
+     * @return
      */
-    public static Boolean isValidString(String str){
-        
+    public static Boolean isValidString(String str) {
         return str != null && !str.isEmpty() && !str.trim().isEmpty();
     }
-    
-    
+
 }
